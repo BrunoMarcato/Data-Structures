620e057ee24eec03b0bdd4ea27465880d1d1117b
Bruno Marcato <brunomarcato11@gmail.com>
2021-09-28 20:14:11 -0300

    Primeiro exercício

--- exerciciosRevisao/ex01.c
+++ exerciciosRevisao/ex01.c
@@ -15,21 +15,27 @@ void formatarMinutoHora(int totalMinutos, int *minutos, int *horas) {
     while(totalMinutos >= 60) { 
         totalMinutos -= 60;
         ++*horas;
-    }
+    }//while
+
     //a variável minutos recebe o restante dos minutos que não foram incluidos na estrutura while, caso existam.
     *minutos = totalMinutos;
+    
 }//formatarMinutoHora
 
 int main(void) {
     //declaração de variáveis
     int minutos_totais, minutos_formatados, horas_formatadas;
+
     //solicitação dos minutos totais a serem formatados
     printf("Digite o total de minutos: ");
     scanf("%d", &minutos_totais);
+
     //chamada da função, usando o operador & para se referir ao endereço de memória das variáveis que devem ser passadas por referência
     formatarMinutoHora(minutos_totais, &minutos_formatados, &horas_formatadas);
+
     //exibição do total de minutos formatados
     printf("\n%dh:%dmin", horas_formatadas, minutos_formatados);
+
     return 0;
 }//main
 
