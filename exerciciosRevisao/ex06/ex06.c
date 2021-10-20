4a9f128ae59dfa2b3a9cffa9a63e4ab925b3ba7a
BrunoMarcato <78366277+BrunoMarcato@users.noreply.github.com>
2021-10-20 13:13:54 -0300

    Update

--- exerciciosRevisao/ex06/ex06.c
+++ exerciciosRevisao/ex06/ex06.c
@@ -5,11 +5,6 @@
     @brief methods archive of exercise 6
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
-#include <conio.h>
 #include "ex06.h"
 
 //global variables
